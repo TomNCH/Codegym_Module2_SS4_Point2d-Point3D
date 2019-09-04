2c43c8b7972bd5afc1f72df170ee3b780082a405
HieuNC <nguyenchihieu.10390@gamil.com>
2019-09-04 11:31:29 +0700

    point2d point3d

---
+++ Point2DandPoint3D/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(1,2);
+        Point3D point3D = new Point3D(3,4,5);
+
+        System.out.println(Arrays.toString(point2D.getXY()));
+        System.out.println(point2D.toString());
+
+        System.out.println(Arrays.toString(point3D.getXYZ()));
+        System.out.println(point3D.toString());
+    }
+}
